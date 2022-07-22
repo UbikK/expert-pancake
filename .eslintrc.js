56c7114c7e1cece3bd34fc238150fcd207f91137
UbikK <cohen.alberic@gmail.com>
2022-07-22 09:31:17 +0200

    tech-linter and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 module.exports = {
-  root: true,
-  extends: [
-    '@react-native-community',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'typescript',
-    'prettier'
-  ],
-  //parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        semi: true,
-        singleQuote: true,
-        printWidth: 80,
-        embeddedLanguageFormatting: 'auto',
-        singleAttributePerLine: true
-      }
+    root: true,
+    extends: [
+        '@react-native-community',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'typescript',
+        'prettier'
     ],
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off'
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off'
-      }
-    }
-  ]
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                semi: true,
+                tabWidth: 4,
+                singleQuote: true,
+                printWidth: 80,
+                embeddedLanguageFormatting: 'auto',
+                singleAttributePerLine: true
+            }
+        ],
+        'arrow-body-style': 'off',
+        'prefer-arrow-callback': 'off'
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off'
+            }
+        }
+    ]
 };
